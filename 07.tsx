a21c5b04b472ced3c2d0157052317f4cd691a7eb
Mateus nascimento <mateus.nasciment@hotmail.com>
2023-01-10 05:16:56 -0300

    fix : Math.floor

--- 07.tsx
+++ 07.tsx
@@ -1,2 +1,8 @@
 export function cockroachSpeed(s: number): number{ s = s * 1000 * 1000 / 3600; return s}
  código  não está passando no ultimo teste 
+
+
+
+ #codigo arrumado  com método Math.floor
+ export function cockroachSpeed(s: number): number {return Math.floor(s * 100 * 1000 / 3600);}
+estava espectando o valor de 30 e estava retornando 3000
