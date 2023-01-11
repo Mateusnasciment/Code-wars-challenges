36b0ff80240a6d8753d84f9158855d93a2882f58
Mateus nascimento <mateus.nasciment@hotmail.com>
2023-01-11 08:40:05 -0300

    WIP: parseInt

---
+++ 08.tsx
@@ -0,0 +1,6 @@
+export function stringToNumber(str: string): number { return ParseInt(str = ''); }
+
+
+
+export function stringToNumber(str: string): number { return ParseInt(str ""); }
+nessa questão usei o método parseInt para converter a string em number 
