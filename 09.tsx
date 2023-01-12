3ed8bdfd5107e81325df7c8789054fac6d609d22
Mateus nascimento <mateus.nasciment@hotmail.com>
2023-01-12 07:06:18 -0300

    feat: StrReverse

---
+++ 09.tsx
@@ -0,0 +1,21 @@
+const strReverse = (str: string) => str.split("").reverse().join("")
+
+export function spinWords(words: string): string {
+    
+    const splitedWorlds = words.split(" ")
+    const newWords = []
+
+    for(let word of splitedWorlds) {
+        const newWord = word.length >= 5 ? strReverse(word) : word
+    
+        newWords.push(newWord)
+    }
+
+    return newWords.join(" ")
+
+
+    // Este código define uma função que recebe uma string como entrada e retorna uma nova string com as palavras de tamanho maior ou igual a 5 invertidas.
+    //  Ele faz isso dividindo a string de entrada em um array de palavras, itera sobre cada palavra e,
+    //   se a palavra tiver 5 ou mais caracteres, usa a função "strReverse" para invertê-la. 
+    //  A função "strReverse" usa métodos como "split", "reverse" e "join" para dividir, inverter e juntar a string invertida.
+    //  Após iterar sobre todas as palavras, a função "spinWords" junta as palavras invertidas e não invertidas de volta em uma string e retorna esse valor.
