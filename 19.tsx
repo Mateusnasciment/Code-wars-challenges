2e961be1262e66f9517d46b3258f9a356885c177
Mateus nascimento <mateus.nasciment@hotmail.com>
2023-02-07 19:48:04 -0300

    wip: split.reverse

---
+++ 19.tsx
@@ -0,0 +1,18 @@
+Complete the solution so that it reverses all of the words within the string passed in.
+
+Words are separated by exactly one space and there are no leading or trailing spaces.
+
+Example(Input --> Output):
+
+"The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+
+/**
+ * We split the string into an array of words, then we map over each word, reverse it, and join it back
+ * together. Then we join the array of words back together
+ * @param {string} str - string
+ * @returns A function that takes a string and returns a string.
+ */
+export function reverseWords(str: string): string {
+    return str.split(" ").map(word => word.split("").reverse().join("")).join(" ");
+  }
