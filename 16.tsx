0f1e921fdb0b0273f4eb95aa34445d53e13475ce
Mateus nascimento <mateus.nasciment@hotmail.com>
2023-02-28 23:34:01 -0300

    chore: descrição

--- 16.tsx
+++ 16.tsx
@@ -6,4 +6,10 @@
 export const isPangram = (phrase: string): boolean => {
     let isPangram = /^[A-Za-z]+$/;
       return phrase.match(isPangram)?true:false;
-  }
+  } 
+
+
+  essa const recebe uma string 
+  que passa um regex em todas as letras do alfabeto 
+  se a string passar no regex retorna true
+  
