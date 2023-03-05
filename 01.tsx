a04da90c0a86542077e84e72da7407b9affbea58
Mateus nascimento <mateus.nasciment@hotmail.com>
2023-03-05 19:21:41 -0300

    descripition 01

--- 01.tsx
+++ 01.tsx
@@ -18,3 +18,12 @@ export function countSheeps(arrayOfSheep: Array<Boolean>) {
 // The correct answer would be 17.
 
 // Hint: Don't forget to check for bad values like null/undefined
+Claro! Essa função é utilizada para contar o número de ovelhas (sheep, em inglês) presentes em uma lista.
+
+Ela recebe como parâmetro uma lista de valores booleanos, onde cada valor representa se uma ovelha está presente ou não. Por exemplo, se o valor é true, significa que a ovelha está presente, e se o valor é false, significa que ela está ausente.
+
+A função percorre a lista e verifica se ela está vazia ou não é uma lista (utilizando a função Array.isArray). Caso isso seja verdade, a função retorna o valor 0, indicando que não há nenhuma ovelha presente.
+
+Se a lista não estiver vazia e for uma lista válida, a função utiliza a função filter do JavaScript para filtrar os valores booleanos true da lista e retorna o comprimento (length) do novo array resultante, que representa o número de ovelhas presentes na lista.
+
+Dessa forma, a função pode ser utilizada para contar o número de ovelhas presentes em uma lista e retornar esse valor como resultado.
