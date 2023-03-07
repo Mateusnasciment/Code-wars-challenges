cc027a4f3450c2736a792e234879e0cf511cc589
Mateus nascimento <mateus.nasciment@hotmail.com>
2023-03-06 23:18:14 -0300

    description : findPair

--- 21.ts
+++ 21.ts
@@ -18,3 +18,12 @@ function findPair(numbers: number[], sum: number): [number, number] | null {
   // If not found, return null
   return null;
 }
+Esse código é uma função que recebe dois argumentos: um array de números e um número que representa a soma que estamos procurando. O objetivo da função é encontrar dois números no array que, quando somados, resultam nessa soma.
+
+A primeira coisa que a função faz é criar um mapa vazio que será usado para armazenar cada número do array e seu índice correspondente. Em seguida, a função percorre o array de números usando um loop for.
+
+Para cada número do array, a função subtrai esse número da soma que estamos procurando para obter o complemento. Em seguida, verifica se esse complemento já existe no mapa que criamos antes. Se existir, significa que encontramos um par de números cuja soma é a soma que estamos procurando, então a função retorna esses dois números.
+
+Se o complemento não existir no mapa, a função armazena o número atual e seu índice no mapa. Em outras palavras, ela está "marcando" esse número como tendo sido visto antes.
+
+Se a função percorrer todo o array sem encontrar um par de números cuja soma seja a soma que estamos procurando, ela retorna null, indicando que não encontrou nenhum par.
